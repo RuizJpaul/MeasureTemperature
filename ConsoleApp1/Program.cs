774c4936acd37929ebbb8082028da8491be1bdf4
RuizJpaul <117695167+RuizJpaul@users.noreply.github.com>
2022-12-29 17:56:24 -0500

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int realNum;
+            string ans;
+            Console.Write("Enter a temperature to evaluate: ");
+            bool num = int.TryParse(Console.ReadLine(), out realNum);
+            // we use a reduced conditional structure (developed if)
+            // answer = condition ? "(actions in case True)" : "(actions in case False)";
+            ans = num == true ? True(realNum) : "that isn't a correct temperature";
+            Console.WriteLine(ans);
+        }
+        public static string True(int temperature)
+        {
+            string clima;
+            clima = temperature <= 15 ? "It's so cold" : temperature >= 16 && temperature <= 28 ?
+                "It's a friendly weather" : "It's so hot"; 
+            return clima;
+        }
+    }
+}
